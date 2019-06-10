4f18d7a9bc5614662413644f891b16a6060009c9
archaeon <harry.wykman@riseup.net>
2019-06-10 13:37:14 +0800

    part 4 complete

--- engine.py
+++ engine.py
@@ -2,8 +2,9 @@ import tcod as libtcod
 
 from entity import Entity
 from map_objects import game_map as gm
-import input_handlers as ih
-import render_functions as rf
+import fov_functions as fovfs
+import input_handlers as ihs
+import render_functions as rfs
 
 
 def main():
@@ -16,9 +17,15 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+    fov_algorithm = 0
+    fov_light_walls = True
+    fov_radius = 10
+
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150)
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'light_wall': libtcod.Color(130, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
     }
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@", libtcod.white)
@@ -38,19 +45,28 @@ def main():
     game_map = gm.GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
 
+    fov_recompute = True
+
+    fov_map = fovfs.initialize_fov(game_map)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        rf.render_all(con, entities, game_map, screen_width, screen_height, colors)
+        if fov_recompute:
+            fovfs.recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+
+        rfs.render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+
+        fov_recompute = False
 
         libtcod.console_flush()
 
-        rf.clear_all(con, entities)
+        rfs.clear_all(con, entities)
 
-        action = ih.handle_keys(key)
+        action = ihs.handle_keys(key)
 
         move = action.get("move")
         exit = action.get("exit")
@@ -60,6 +76,7 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy):
                 player.move(dx, dy)
+                fov_recompute = True
 
         if exit:
             return True
